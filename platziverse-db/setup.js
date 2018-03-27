c3f7dfe35cfe1498759790f238eab76334fa13b7
lilicolls <lilianacolls@gmail.com>
2018-03-27 21:13:25 +0000

    agregando advertencia sobre el borrado de la base de datos utilizando prompt para confirmacion del usuario

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -5,18 +5,36 @@ const db = require('./')
 const inquirer = require('inquirer')
 const chalk = require ('chalk')
 
+const prompt = inquirer.createPromptModule()  //devuelve una promesa que se cumple cuando el usuario responda el prompt
 async function setup () {
+
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup){  
+    //Si el usuario responde no, se hace un early return, de lo contrario se continua con el resto del script de setup
+    return console.log("nothin happened :)")
+  }
+  
   // obj de configuracion para la bdd de acuerdo a los requisitos de sequelize
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
-    password: process.env.DB_PASSS || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: s => debug(s),     //para utilizar el modulo debug de npm
     setup: true,
     operatorsAliases: false
   }
+  console.log('el usuario puso ' + config.password)
+
+
   await db(config).catch(handleFatalError)
   console.log('succes')
   
@@ -24,7 +42,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error( `${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
